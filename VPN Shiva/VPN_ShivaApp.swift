9ef26f6bc58a545ec8c815779b453757553861bb
AntonShiva <ryashinanthon@yandex.ru>
2025-01-30 11:39:54 +0300

    Initial Commit

---
+++ VPN Shiva/VPN_ShivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPN_ShivaApp.swift
+//  VPN Shiva
+//
+//  Created by Anton Rasen on 30.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct VPN_ShivaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
